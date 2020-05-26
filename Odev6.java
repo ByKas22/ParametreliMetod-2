f3c5eef1057611ac238c27b9e1a6899de233114b
Yunus Kavcuk <65766059+ByKas22@users.noreply.github.com>
2020-05-26 15:14:40 +0300

    Update Odev6.java

--- Odev6.java
+++ Odev6.java
@@ -1,55 +1,49 @@
+/* Daha önceden notları belirli olan öğrencileri parametreli metod kullanarak notu öğrenmek istediğimiz öğrencinin ismini
+girdiğimizde ekranda o öğrencinin notunu ekrana yazdıran program */
 
 package odev.pkg6;
-
-import java.util.Scanner;
-
+import java.util.Scanner; //scanner sınıfını kütüphaneye ekledik.
 public class Odev6 
 {
     public static void main(String[] args) 
     {
-        String ogrenci;
-        
-        Scanner girdi =new Scanner(System.in);
-        System.out.println("Sınıftaki Öğrenciler:\n Yunus,\n Emre,\n Metin,\n Furkan,\n Ahmet,\n Mehmet\n");
-        System.out.println("Notu Öğrenmek İstediğiniz Öğrencinin Adını Giriniz..:");
-        ogrenci =girdi.nextLine();
-        ogrenciler(ogrenci);
-        
-        
+        String öğrenci; //String türünde değişken oluşturduk.
+        Scanner girdi =new Scanner(System.in); //değer girmemizi sağlayan scanner sınıfını çağırdık.
+System.out.println("Sınıftaki Öğrenciler:\n Yunus,\n Emre,\n Metin,\n Furkan,\n Ahmet,\n    Mehmet\n"); ekranda görmemizi sağladık.
+        	System.out.println("Notu Öğrenmek İstediğiniz Öğrencinin Adını Giriniz..:"); ekranda metni.
+        ogrenci =girdi.nextLine(); //klavyeden değer girdik.
+        ogrenciler(ogrenci); //öğrenciler metodunu çağırdık.
     }
-    static String ogrenciler(String ogrenci)
+    static String ogrenciler(String ogrenci) //öğrenciler adında metod oluşturduk.(parametreli)
     {
-    int Yunus=50,Emre=90,Metin=75,Furkan=80,Ahmet=77,Mehmet=40;    
+    int Yunus=50,Emre=90,Metin=75,Furkan=80,Ahmet=77,Mehmet=40;  //değişkenleri oluşturduk.  
         
-    switch(ogrenci)
+    switch(ogrenci) //koşul ifadesi oluşturduk.
     {
-        case "Yunus":
-            System.out.println("Yunus'un Notu..:"+Yunus);
-        break;
+        case "Yunus": //eğer ki Yunus yazar ise;
+            System.out.println("Yunus'un Notu..:"+Yunus); //ekranda yazacak olan metin.
+        break; //işlemi bitirdik.
+        case "Emre": //eğer ki Yunus yazar ise;
+            System.out.println("Emre'nin Notu..:"+Emre); //ekranda yazacak olan metin.
+        break; //işlemi bitirdik.
+        case "Metin": //eğer ki Metin yazar ise;
+            System.out.println("Metin'in Notu..:"+Metin); //ekranda yazacak olan metin.
+        break; //işlemi bitirdik.
         
-        case "Emre":
-            System.out.println("Emre'nin Notu..:"+Emre);
-        break;
+        case "Furkan": //eğer ki Furkan yazar ise;
+            System.out.println("Furkan'nın Notu..:"+Furkan); //ekranda yazacak olan metin.
+        break; //işlemi bitirdik.
+        case "Ahmet": //eğer ki Ahmet yazar ise;
+            System.out.println("Ahmet'in Notu..:"+Ahmet); //ekranda yazacak olan metin.
+        break; //işlemi bitirdik.
         
-        case "Metin":
-            System.out.println("Metin'in Notu..:"+Metin);
-        break;
+        case "Mehmet": //eğer ki Mehmet yazar ise;
+            System.out.println("Mehmet'in Notu..:"+Mehmet); //ekranda yazacak olan metin.
+        break; //işlemi bitirdik.
         
-        case "Furkan":
-            System.out.println("Furkan'nın Notu..:"+Furkan);
-        break;
-        
-        case "Ahmet":
-            System.out.println("Ahmet'in Notu..:"+Ahmet);
-        break;
-        
-        case "Mehmet":
-            System.out.println("Mehmet'in Notu..:"+Mehmet);
-        break;
-        
-        default: 
-                System.out.println("Listedeli Öğrencilerden Birini Giriniz");
+        default: //eğer ki hiçbir koşul sağlanmaz ise;  
+                System.out.println("Listedeli Öğrencilerden Birini Giriniz"); //ekranda yazar.
     }   
-     return ogrenci;   
+     return ogrenci; //geri değer döndürdük.  
     }
 }
